9a9acc19fe49631e370017008dc1c04cd2e3743c
Howard-00 <yywsyywsyyws@gmail.com>
2019-02-28 07:58:58 +0800

    完成了running letter

---
+++ level1/p01_runningLetter/running.cpp
@@ -0,0 +1,25 @@
+﻿#include <cstdio>
+#include <cstdlib>
+#include <unistd.h>
+using namespace std;
+//Tested on Linux
+#define clean() printf("\033[2J");
+int main() {
+  int f = 0, p = 0;
+  while (1) {
+    if (p == 20)
+      f = 1;
+    if (p == 0)
+      f = 0;
+    sleep(1);
+    clean();
+    for (int i = 0; i < p; ++i)
+      printf(" ");
+    puts("Howard");
+    if (f)
+      --p;
+    else
+      ++p;
+  }
+  return 0;
+}
