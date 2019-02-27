8423fb6f1cd26cb75de4d7afdab973c9cb393f67
me1t0ut <meltout@outlook.com>
2019-02-27 21:04:00 +0800

    Diophantus down

---
+++ level1/p03_Diophantus/Diophantus.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	for (int age = 0; age <= 200; ++age)
+	{
+		if (age % 12 == 0 && age % 7 == 0 && age - age / 6 - age / 12 - age / 7 - 5 - 4 == age / 2) 
+		{
+			printf("age = %d\n", age - 4);
+			break;
+		}
+	}
+	return 0;
+}
