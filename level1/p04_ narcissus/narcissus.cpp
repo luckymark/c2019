b6559d9df3c74a5dc46733d9b36fbc083be86d7e
Howard Cheung <hc@hc-Inspiron-7472.localdomain>
2019-02-28 16:52:04 +0800

    已完成narcissus

---
+++ level1/p04_ narcissus/narcissus.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+int main() {
+  int f = 0;
+  for (int i = 1; i <= 9; ++i)
+    for (int j = 0; j <= 9; ++j)
+      for (int k = 0; k <= 9; ++k)
+        if (i * i * i + j * j * j + k * k * k == i * 100 + j * 10 + k) {
+          if (f)
+            printf(" ");
+          else
+            f = 1;
+          printf("%d%d%d", i, j, k);
+        }
+  putchar('\n');
+}
