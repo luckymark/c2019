8ef6d7d7de0af95216b02bae1cf45aeb69325ad7
兼桑 <tiki@jiansangdemacbook-air.local>
2019-02-28 11:40:10 +0800

    Initial Commit

---
+++ lll/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lll
+//
+//  Created by 兼桑 on 2019/2/28.
+//  Copyright © 2019 兼桑. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
