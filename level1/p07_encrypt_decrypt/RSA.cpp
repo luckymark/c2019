19eaaac1a5bbea1876202e8ce762a090d3e4c0d4
HummaWhite <48054481+HummaWhite@users.noreply.github.com>
2019-02-28 23:07:27 +0800

    encryption and decryption

--- level1/p07_encrypt_decrypt/RSA.cpp
+++ level1/p07_encrypt_decrypt/RSA.cpp
@@ -1,1 +1,78 @@
+#include<iostream>
+#include<cstring>
+#include<cstdio>
+#include<vector>
+typedef long long LL;
+
+using namespace std;
+
+LL P = 43, Q = 59, E = 13;
+LL N = P * Q;
+LL PHI_N = (P-1) * (Q-1);
+
+int mLength, cLength;
+
+vector<LL> cipherText;
+
+void exgcd(LL a, LL b, LL &x, LL &y)           //扩展欧几里得算法，求逆元
+{
+	if (b == 0)
+	{
+		x = 1, y = 0;
+		return;
+	}
+	exgcd(b, a%b, x, y);
+	LL tmp = y;
+	y = x - y * (a / b);
+	x = tmp;
+}
+
+LL qpow(LL x, LL n, LL mod)                    //快速幂，计算 x^n % mod的值
+{
+	LL ret = 1;
+	while (n)
+	{
+		if (n&1)
+			ret = (ret * x) % mod;
+		x = (x * x) % mod;
+		n >>= 1;
+	}
+	return ret;
+}
+
+void encrypt(string text)
+{
+	int len = text.length();
+	for (int s = 0; s < len; s++)
+	{
+		LL val = qpow(text[s], E, N);     //加密，C = M^E % N
+		cipherText.push_back(val);
+	}
+}
+
+void decrypt(string &res)                     
+{
+	LL D, F;
+	exgcd(E, PHI_N, D, F);   //D是 DE===1 (mod phi(N)) 即E对于(P-1)*(Q-1)的逆元
+		res += val;
+	if (D < 0)
+		D += PHI_N;
+	for (int s = 0; s < cipherText.size(); s++)
+	{
+		LL val = qpow(cipherText[s], D, N);  //解密，M = C^D % N
+	}
+}
+
+int main()
+{
+	string message;
+	getline(cin, message);
+	encrypt(message);
+	cout << "Encryped number string:\n";
+	for (int s = 0; s < cipherText.size(); s++)
+		cout << cipherText[s] << " ";
+	string decoded;
+	decrypt(decoded);
+	cout << "\nDecryped message:\n" << decoded << endl;
+}
 
--- level1/p07_encrypt_decrypt/base64.cpp
+++ level1/p07_encrypt_decrypt/base64.cpp
@@ -1,1 +1,80 @@
+#include<iostream>
+#include<cstring>
+#include<cstdio>
+#include<vector>
+
+using namespace std;
+
+int Table[] = {
+	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+	'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+	'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+	'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
+};
+
+string encodedText, decodedText;
+
+void pr(int k)
+{
+	for (int s=0; s<6; s++) printf("%d", (k>>s)&1);
+	printf("\n");
+}
+
+void encode(string text)
+{
+	int len = text.length();
+	vector<int> tmp;
+	for (int s = 0; s < len; s++)
+	{
+		for (int t = 7; t >= 0; t--)
+			tmp.push_back((text[s] >> t) & 1);
+	}
+	int size = tmp.size();
+	while (size % 24 != 0)
+	{
+		tmp.push_back(0);
+		size++;
+	}
+	for (int s = 0; s < size; s += 6)
+	{
+		int val = 0;
+		for (int t = 0; t < 6; t++)
+			val += tmp[s+t] << (5-t);
+		if (val)
+			encodedText += Table[val];
+		else
+			encodedText += '=';
+	}
+}
+
+void decode(string text)
+{
+	int len = text.length();
+	vector<int> tmp;
+	for (int s = 0; s < len; s++)
+	{
+		if (text[s] == '=') continue;
+		int inc;
+		for (inc = 0; text[s] != Table[inc]; inc++);
+		for (int t = 0; t < 6; t++)
+			tmp.push_back((inc >> (5-t)) & 1);
+	}
+	for (int s = 0; s < tmp.size(); s += 8)
+	{
+		int val = 0;
+		for (int t = 0; t < 8; t++)
+			val += tmp[s+t] << (7-t);
+		decodedText += val;
+	}
+}
+
+int main()
+{
+	string message;
+	getline(cin, message);
+	encode(message);
+	cout << "Encoded:\n" << encodedText << endl;
+	decode(encodedText);
+	cout << "Decoded:\n" << decodedText << endl;
+}
 
