d430e23548f86e0ab4d6f796b2d5ccbd2fd3f797
傅阳烨 <1093928135@qq.com>
2019-03-01 15:55:10 +0800

    完成了Diophantus

---
+++ Diophantus.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int nf=5,ns; //nf:age of Diophantus		ns:age of his son
+	while((nf/6+nf/12+nf/7+5+ns+4)!=nf||nf%12!=0||nf%7!=0)
+	{
+		++nf;
+		ns=nf/2;
+	}
+	printf("Diophantus\' age is %d.\n",nf);
+	return 0;
+}
