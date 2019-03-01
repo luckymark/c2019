bff63d2e185bfd0981d814510da9051fdafca536
Hcy-Horst <48054538+Hcy-Horst@users.noreply.github.com>
2019-03-01 21:58:19 +0800

    Create runningLatter.cpp

---
+++ level1/p01_runningLetter/runningLatter.cpp
@@ -0,0 +1,30 @@
+#include<cstdio>
+#include<cstdlib>
+
+const int _Maxlength=80
+const int time=5000000
+
+using namespace std;
+void sleep(){
+	for (int i=1;i<=time;i++){
+	}
+	return;
+}
+int abs(int x){
+	return x>0 ? x : -x;
+}
+int main(){
+	int blank;
+	for(int i=0;i<2*_Maxlength;i++){
+			sleep();
+			blank=_Maxlength-abs(_Maxlength-i);
+			for(int j=0;j<blank;j++) 
+      		printf(" ");
+			printf("8\n");
+			sleep();
+			system("cls");
+	}
+	return 0;
+}
+
+
