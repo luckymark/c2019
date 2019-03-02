b4480b193a0b6d00e74054e1716b9b3fda0ffd7f
Decision2016 <787362202@qq.com>
2019-03-02 09:11:25 +0800

    修改runningLetter部分

--- level1/p01_runningLetter/runningLetter.cpp
+++ level1/p01_runningLetter/runningLetter.cpp
@@ -3,7 +3,6 @@
 #include<windows.h>
 #include<cstdlib>
 
-
 #define W 80       //设置运行宽度 
 #define SLEEP_TIME 50     //设置休眠时间 
 
@@ -14,13 +13,17 @@ int main(){
 	while(1){
 		for(int i = 0;i<W - len;i++){
 			system("cls");
-			for(int j = 1;j<=i;j++) std::cout<<" ";
+			for(int j = 1;j<=i;j++){
+			    std::cout<<" ";
+			}
 			std::cout<<s;
 			Sleep(SLEEP_TIME);
 		}
 		for(int i = W - len;i>=0;i--){
 			system("cls");
-			for(int j = 1;j<=i;j++) std::cout<<" ";
+			for(int j = 1;j<=i;j++){
+				std::cout<<" ";
+			}
 			std::cout<<s;
 			Sleep(SLEEP_TIME);
 		}
