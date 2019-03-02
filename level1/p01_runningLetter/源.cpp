ef4092bd790cd7633cce5c9dae6ae3f9bf64e6a0
1838143204 <48073519+1838143204@users.noreply.github.com>
2019-03-02 19:49:38 +0800

    Update 源.cpp

--- level1/p01_runningLetter/源.cpp
+++ level1/p01_runningLetter/源.cpp
@@ -2,26 +2,20 @@
 #include<stdlib.h>
 #include<windows.h>
 int LENGTH = 70;
-int main()
-{
+int main(){
 	int k, i = 1, l, p;
-	while (i++)
-	{
+	while (i++){
 		if (i > LENGTH)l = i % LENGTH;
 		else l = i;
 		p = i / LENGTH;
-		if (p % 2 == 0)
-		{
-			for (k = 1; k < l - 1; k++)
-			{
+		if (p % 2 == 0){
+			for (k = 1; k < l - 1; k++){
 				printf_s(" ");
 			}
 			printf_s("->");
 		}
-		if (p % 2 == 1)
-		{
-			for (k = 1; k < LENGTH - l - 1; k++) 
-			{
+		if (p % 2 == 1){
+			for (k = 1; k < LENGTH - l - 1; k++) {
 				printf_s(" ");
 			}
 			printf_s("<-");
