bec1370fe361ce7501ccafe06e847ee13f4b488f
=stan66667 <=1084096585@qq.com>
2019-03-02 13:06:43 +0800

    完成了running letter

---
+++ level1/p01_runningLetter/L1T1.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#define Length 20
+
+int main ()
+{
+	int a=0;
+	int b=1;
+	while(1)
+	{
+		a+=b;
+		system("cls");
+		int i=0;
+		while(i<a)
+		{
+			printf(" ");
+			i++;
+		}
+			printf("H");
+			Sleep(100);
+			if(a==Length)
+			{
+			   b=-1;
+			}
+			if(a==0)
+			{
+				b=1;
+			}
+	}
+    return 0;		
+}
+	
+
+ 
+
