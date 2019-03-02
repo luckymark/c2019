54d7aa3571be61c5ee5e4cfbaffb08f594007c85
2w6f8c <2428391347@qq.com>
2019-03-02 15:46:29 +0800

    刘露 完成了narcissus

---
+++ level1/p04_ narcissus/Narcissus.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int i;
+    int a, b, c;
+    for (i = 100; i < 999; i++) {
+        a = i / 100;
+        b = (i - a * 100) / 10;
+        c = i % 10;
+
+        if (a * a * a + b * b * b + c * c * c == i) printf("%d\t", i);
+    }
+
+    printf("\n");
+    system("pause");
+    return 0;
+}
