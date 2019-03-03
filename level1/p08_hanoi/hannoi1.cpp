09d23dbc4281b9b2dda9ab341c9db88d9ea5f658
unknown <xdx18764331633@163.com>
2019-03-03 23:00:26 +0800

    提交作业

--- level1/p08_hanoi/hannoi1.cpp
+++ level1/p08_hanoi/hannoi1.cpp
@@ -1,10 +1,7 @@
 #include <stdio.h>
-#define N 3
+#define N 64 
 void exchange(int n, char a, char b, char c) ;
 int main(){
-//	char a='a';
-//	char b='b';
-//	char c='c';
 	exchange(N, 'a', 'b','c');
 	
 }
