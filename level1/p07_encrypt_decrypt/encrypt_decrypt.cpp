7b3e0710303ddb830043ff3cef22d0bc47464602
Howard Cheung <hc@hc-Inspiron-7472.localdomain>
2019-03-03 21:20:20 +0800

    完成encrypt_decrypt

---
+++ level1/p07_encrypt_decrypt/encrypt_decrypt.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int encrypt() {
+  cout << "请输入要加密的内容" << endl;
+  string origin;
+  cin >> origin;
+  char encrypted[2 * origin.length() + 5];
+  getchar();
+  for (int i = 0; i < origin.length(); ++i) {
+    int e = origin[i] * 13 + 14;
+    encrypted[2 * i + 1] = e / 95 + 31;
+    encrypted[2 * i] = e % 95 + 31;
+  }
+  cout << "加密后的内容为：\n" << encrypted << endl;
+  return 0;
+}
+int decrypt() {
+  cout << "请输入加密后的内容" << endl;
+  string encrypted;
+  cin >> encrypted;
+  char origin[encrypted.length() / 2 + 5];
+  getchar();
+  for (int i = 0; i < encrypted.length() / 2; ++i) {
+    int e = encrypted[2 * i] + encrypted[2 * i + 1] * 95 - 96 * 31;
+    origin[i] = (e - 14) / 13;
+  }
+  cout << "原内容为：\n" << origin << endl;
+  return 0;
+}
+void leave() {
+  cout << "非法指令，退出。。。" << endl;
+  exit(0);
+}
+int main() {
+  cout << "请选择操作\n1)    加密\n2)    解密\n其他) 退出" << endl;
+  char item = getchar();
+  switch (item) {
+    case '1':
+      if (encrypt()) cout << "程序出错" << endl;
+      main();
+    case '2':
+      if (decrypt()) cout << "程序出错" << endl;
+      main();
+    default:
+      leave();
+  }
+}
