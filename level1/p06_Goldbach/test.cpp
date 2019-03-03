b57e2c43e672313cbbe080fb7d0bb29d1ac4b425
odygrag <odygrag@gmail.com>
2019-03-03 23:37:02 +0800

    完成了一些题

---
+++ level1/p06_Goldbach/test.cpp
@@ -0,0 +1,33 @@
+﻿#include<stdio.h>
+
+int prime[50] = { 2 };		// 用于存放找到的素数
+
+inline bool isRight(int &n) {
+	for (int i = 0; prime[i]; ++i)
+		for (int j = 0; prime[j]; ++j)
+			if (prime[i] + prime[j] == n)
+				return true;
+	return false;
+}
+
+int main() {
+	int i;			// 循环专用变量
+
+	// 得到 100 之内的所有素数
+	for (int n = 3; n < 1000; ++n) {
+		for (i = 0; prime[i]; ++i)
+			if (0 == n % prime[i])
+				break;
+		if (!prime[i])
+			prime[i] = n;
+	}
+	// 遍历验证猜想
+	for (i = 4; i < 100; i += 2)
+		if (!isRight(i)) {
+			printf("Goldbach is wrong!");
+			break;
+		}
+	if (100 == i)
+		printf("Goldbach is right!");
+	return 0;
+}
---
+++ level1/p07_encrypt_decrypt/test.cpp
@@ -0,0 +1,46 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+inline void encrypt(string &s) {
+	for (int i = 0; i < s.length(); ++i) {
+		if (i % 2)
+			s[i] += i % 8;
+		else
+			s[i] -= i % 7;
+	}
+	cout << "The encrypted text is:\n" << s << endl;
+}
+
+inline void decrypt(string &s) {
+	for (int i = 0; i < s.length(); ++i) {
+		if (i % 2)
+			s[i] -= i % 8;
+		else
+			s[i] += i % 7;
+	}
+	cout << "The decrypted text is:\n" << s << endl;
+}
+
+int main() {
+	char option;
+	string s;
+	while (true) {
+		cout << "Choose to ENCRYPT or DECRYPT (D / E):\n";
+		cin.get(option);
+		cin.get();
+		if ('d' == option || 'D' == option) {
+			cout << "Enter a string to decrypt:\n";
+			getline(cin, s);
+			decrypt(s);
+		}
+		else if ('e' == option || 'E' == option) {
+			cout << "Enter a string to encrypt:\n";
+			getline(cin, s);
+			encrypt(s);
+		}
+		else
+			cout << "Invalid option! Try again:\n";
+	}
+	return 0;
+}
