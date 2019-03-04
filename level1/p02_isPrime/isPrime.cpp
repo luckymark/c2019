82826c6f977241b751b7ba15cdf60551572d59b3
Baiyusheng <48091182+Baiyusheng@users.noreply.github.com>
2019-03-04 13:02:47 +0800

    完成了isPrime

---
+++ level1/p02_isPrime/isPrime.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	
+	if (n == 1)
+	printf("%d is Prime Number.", n);
+	
+	if(n != 1 && n%2!=0 && n%3!=0 && n%5!=0 && n%7!=0)
+	printf("%d is Prime Number.", n);
+	else
+	printf("%d is not Prime Number.", n);
+	
+	return 0;
+ } 
