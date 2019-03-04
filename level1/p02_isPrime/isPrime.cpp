fa9bf8c845c5de91cbe7f0de128b4a8fe5e9f647
Baiyusheng <48091182+Baiyusheng@users.noreply.github.com>
2019-03-04 13:28:48 +0800

    修改了isPrime部分错误

--- level1/p02_isPrime/isPrime.cpp
+++ level1/p02_isPrime/isPrime.cpp
@@ -5,10 +5,11 @@ int main()
 	scanf("%d", &n);
 	
 	if (n == 1)
+	printf("%d is not Prime Number.", n);
+	
+	else if(n != 1 && n%2!=0 && n%3!=0 && n%5!=0 && n%7!=0)
 	printf("%d is Prime Number.", n);
 	
-	if(n != 1 && n%2!=0 && n%3!=0 && n%5!=0 && n%7!=0)
-	printf("%d is Prime Number.", n);
 	else
 	printf("%d is not Prime Number.", n);
 	
