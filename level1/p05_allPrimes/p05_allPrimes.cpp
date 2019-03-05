9dd5bff4bb66d6e2432f0ad1e8dda2539fa06913
KenNN99 <1292781066@qq.com>
2019-03-05 21:23:52 +0800

    19/3/5 21:35

---
+++ level1/p05_allPrimes/p05_allPrimes.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+	clock_t start, end;
+	start = clock();
+	bool Prime[1001];
+	int i;
+	for (i = 1; i <= 1000; i++)
+	{
+		Prime[i] = true;
+	}
+	for (i = 2; i*i <= 1000; i++)
+	{
+		if (Prime[i])
+		{
+			for (int j = 2; j*i <= 1000; j++)
+			{
+				Prime[j*i] = false;
+			}
+		}
+	}
+	for (i = 1; i <= 1000; i++)
+	{
+		if (Prime[i])
+		{
+			printf("%d\n", i);
+		}
+	}
+	end = clock();
+	double duration = (double)(end - start) / CLOCKS_PER_SEC * 1000;
+	printf("Used %f ms", duration);
+	system("pause");
+	return 0;
+}
