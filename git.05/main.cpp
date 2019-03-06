34a89fa85d60522e0d495584505d4505d5b66427
Candiciiii <1249801158@qq.com>
2019-03-06 19:30:29 +0800

    Initial Commit

---
+++ git.05/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  git.05
+//
+//  Created by 兼桑 on 2019/3/6.
+//  Copyright © 2019 兼桑. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
