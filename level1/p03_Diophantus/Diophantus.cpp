e828bb898c14e3a54edbcc2070b6dbfb10c86d5b
Don-Xu <46224136+Don-Xu@users.noreply.github.com>
2019-03-08 21:36:30 +0800

    Update Diophantus.cpp

--- level1/p03_Diophantus/Diophantus.cpp
+++ level1/p03_Diophantus/Diophantus.cpp
@@ -4,7 +4,7 @@ int main(){
 	for(i=1;i<75;i++){
 		if(2*i%6==0 && 2*i%12==0 && 2*i%7==0){
 			if(i==2*i/6+2*i/12+2*i/7+9){
-				printf("¶ù×ÓËÀÊ±¶ª·¬Í¼%dËê",2*i-4);
+				printf("when his son died, he is %d years old",2*i-4);
 			}
 		}
 	}
