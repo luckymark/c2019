b887f1d35f50f8bc082aaecd730a9228329ee96d
2w6f8c <2428391347@qq.com>
2019-03-08 19:34:43 +0800

    finish PushBoxes

--- level1/p10_pushBoxes/PushBoxes.c
+++ level1/p10_pushBoxes/PushBoxes.c
@@ -3,23 +3,29 @@
 #include <stdlib.h>
 #include <string.h>
 #include <windows.h>
+#include <math.h>
 
 #define NUM_PATH 0
 #define NUM_WALL 1
 #define NUM_PLAYER 2
 #define NUM_BOX 3
 #define NUM_HOLDER 4
+#define NUM_BOX_HOLDER 5
+#define NUM_PLAYER_HOLDER 6
 
 #define DIR_UP 0
 #define DIR_DOWN 1
 #define DIR_LEFT 2
 #define DIR_RIGHT 3
 
-#define MAP_MAX_SIZE 50
+#define MAP_MAX_SIZE 24
 #define HOLDER_MAX_SIZE 10
+#define LEVEL_MAX_SIZE 10
 
-const char* BANNER_PATH = "banner.txt";
-const char* MAP_PATH = "map.txt";
+const char* BANNER_PATH = "banner.txt"; // 恭喜
+const char* MAP_PATH = "map.dat"; // 地图
+const char* RECORD_PATH = "record.dat"; // 记录
+const char* SCORE_PATH = "score.dat"; // 分数
 
 const char* SYMBLE_PATH = "  ";
 const char* SYMBLE_WALL = "▓";
@@ -39,17 +45,46 @@ int rows, cols;
 int currBoxes = 0; 
 // store the positions of holders
 int holders[2][HOLDER_MAX_SIZE];
-int startX, startY;
-int endX, endY;
+int records[LEVEL_MAX_SIZE];
 int playerX, playerY;
+int supposedMoves = 0;
+int currMoves = 0;
+int score = 0;
 int playerWin = 0;
 int holderSize = 0;
 long fileReadPosition = 0L;
+long preFileReadPosition = 0L;
 
+void consoleInit();
+void gotoxy(short x, short y);
+int caclScore(int a, int b);
+int containedInHolders(int x, int y);
+int checkWin();
+void startGame();
+void initMap();
+void readRecords();
+void dumpRecords();
+void dumpScore();
+void printMap();
+void printInfo();
+void printHolders();
+void listenKey();
+void movePlayer(int dir);
+void showMenu();
+void endGame();
+
+int main() {
+    consoleInit();
+    startGame();
+
+    printf("\n");
+    system("pause");
+    return 0;
+}
 
 void consoleInit() {
     system("color 2");
-    // system("mode con cols=60 lines=30");
+    system("mode con cols=40 lines=20");
     CONSOLE_CURSOR_INFO cursor_info = {1, 0};
     SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor_info);
 }
@@ -58,6 +93,49 @@ void gotoxy(short x, short y) {  // 设置光标位置，用于局部刷新，解决闪烁问题
     COORD pos = {x, y};
     HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleCursorPosition(hOut, pos);
+}
+
+void readRecords() {
+    FILE * fp;
+    if((fp = fopen(RECORD_PATH, "r")) == NULL) {
+        printf("error!");
+        return;
+    }
+    // levels should be init first in initMap()
+    for(int i = 0; i < levels; i++) {
+        fscanf(fp, "%d", &records[i]);
+    }
+    fclose(fp);
+}
+
+void dumpRecords() {
+    FILE * fp;
+    if((fp = fopen(RECORD_PATH, "w+")) == NULL) {
+        printf("error!");
+        return;
+    }
+    for(int i = 0; i < levels; i++) {
+        fprintf(fp, "%d ", records[i]);
+    }
+    fclose(fp);
+}
+
+void dumpScore() {
+    FILE * fp;
+    if((fp = fopen(SCORE_PATH, "a+")) == NULL){
+        printf("error!");
+        return;
+    }
+    fprintf(fp, "level%d: %d\n", currLevel, score);
+    fclose(fp);
+}
+
+int caclScore(int a, int b) {
+    if(b >= 2 * a) return 0;
+    if(b <= a) return 100;
+    double ta = a;
+    double tb = b;
+    return round((2 * ta - tb) / ta * 100);
 }
 
 void printMap() {
@@ -74,9 +152,6 @@ void printMap() {
                 case NUM_PLAYER:
                     printf("%s", SYMBLE_PLAYER);
                     break;
-                case NUM_HOLDER:
-                    printf("%s", SYMBLE_HOLDER);
-                    break;
                 case NUM_BOX:
                     printf("%s", SYMBLE_BOX);
                     break;
@@ -88,45 +163,84 @@ void printMap() {
     }
 }
 
-void resetHolders() {
+void printInfo() {
+    gotoxy(0, rows + 1);
+    printf("current level: %d / %d\n",currLevel + 1, levels);
+    printf("supposed moves: %d\n", supposedMoves);
+    printf("current moves: %d\n", currMoves);
+    printf("record: %d\n", records[currLevel]);
+    printf("current score:%3d\n", score = caclScore(supposedMoves, currMoves));
+    printf("press [r] to restart this level");
+}
+
+void printHolders() {
     for(int i = 0; i < holderSize; i++) {
+        // holder上没有box或player则显示holder
         if(map[holders[1][i]][holders[0][i]] == NUM_PATH){
-            map[holders[1][i]][holders[0][i]] = NUM_HOLDER;
             gotoxy(2 * holders[0][i], holders[1][i]);
             printf("%s", SYMBLE_HOLDER);
         }
     }
 }
 
+// 如果需要检测的坐标为holder坐标就返回1
+int containedInHolders(int x, int y) {
+    for (int i = 0 ; i < holderSize; i++) {
+        if(holders[0][i] == x && holders[1][i] == y) return 1;
+    }
+    return 0;
+}
+
 void initMap() {
     FILE* fp;
+    // reset
     holderSize = 0;
+    currMoves = 0;
     // 用r模式读取获取文件当前读取位置会出错
     if ((fp = fopen(MAP_PATH, "rb")) == NULL) {
         printf("error!");
         return;
     }
+    // for rollback
+    preFileReadPosition = fileReadPosition;
     fseek(fp, fileReadPosition, SEEK_SET);
     // 初始化关卡数
-    if(fileReadPosition == 0) fscanf(fp, "%d", &levels);
+    if(fileReadPosition == 0) {
+        fscanf(fp, "%d", &levels);
+        readRecords();
+    }
+    fscanf(fp, "%d", &supposedMoves);
     fscanf(fp, "%d", &rows);
     fscanf(fp, "%d", &cols);
+    int t;
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             fscanf(fp, "%d", &map[i][j]);
             if (map[i][j] == NUM_PLAYER) {
-                playerX = startX = j;
-                playerY = startY = i;
-            }else if(map[i][j] == NUM_HOLDER){
+                playerX = j;
+                playerY = i;
+                // positions of holders are stored in an array, not the map
+            }else if( (t = map[i][j]) == NUM_HOLDER || t == NUM_BOX_HOLDER || t == NUM_PLAYER_HOLDER){
                 holders[0][holderSize] = j; // x
                 holders[1][holderSize] = i; // y
-                holderSize++;
+                ++holderSize;
+                if(t == NUM_HOLDER) {
+                    map[i][j] = 0;
+                }else if(t == NUM_BOX_HOLDER) {
+                    map[i][j] = NUM_BOX;
+                }else if(t == NUM_PLAYER_HOLDER) {
+                    map[i][j] = NUM_PLAYER;
+                    playerX = j;
+                    playerY = i;
+                }
             }
         }
     }
     fileReadPosition = ftell(fp);
     fclose(fp);
     printMap();
+    printHolders();
+    printInfo();
 }
 
 int checkWin() {
@@ -139,6 +253,7 @@ int checkWin() {
 }
 
 void endGame() {
+    playerWin = 1;
     system("cls");
     FILE* fp;
     char strLine[1024];
@@ -151,16 +266,23 @@ void endGame() {
         printf("%s", strLine);
     }
     fclose(fp);
-    system("pause");
 }
 
+
 void showMenu() {
+    int t;
+    if(t = (score > records[currLevel - 1])) {
+        records[currLevel - 1] = score;
+        dumpRecords();
+    }
     if(currLevel >= levels) {
         endGame();
         return;
     }
     system("cls");
     printf("You've passed level%d\n", currLevel);
+    if(t) printf("Congratulations! You've just broken the record!!!\n");
+    printf("Your score is %d, the record is %d\n", score, records[currLevel - 1]);
     printf("[n]: Press n to continue\n");
     printf("[q]: Press q to quit\n");
     char c;
@@ -173,8 +295,8 @@ void showMenu() {
 }
 
 void movePlayer(int dir) {
-    int t;
-    if ((t = map[playerY + DIR_ARR[1][dir]][playerX + DIR_ARR[0][dir]]) == NUM_PATH || t == NUM_HOLDER) {
+    if (map[playerY + DIR_ARR[1][dir]][playerX + DIR_ARR[0][dir]] == NUM_PATH) {
+        ++currMoves;
         map[playerY][playerX] = NUM_PATH;
         gotoxy(2 * playerX, playerY);
         printf("%s", SYMBLE_PATH);
@@ -183,8 +305,11 @@ void movePlayer(int dir) {
         map[playerY][playerX] = NUM_PLAYER;
         gotoxy(2 * playerX, playerY);
         printf("%s", SYMBLE_PLAYER);
-    } else if (map[playerY + DIR_ARR[1][dir]][playerX + DIR_ARR[0][dir]] == NUM_BOX) {
-        if ((t = map[playerY + 2 * DIR_ARR[1][dir]][playerX + 2 * DIR_ARR[0][dir]]) == NUM_PATH || t == NUM_HOLDER) {
+        printInfo();
+        // 有可能box被推到holder上又被推走(或者player从holder上经过)，需要重新打印holder
+        printHolders();
+    } else if (map[playerY + DIR_ARR[1][dir]][playerX + DIR_ARR[0][dir]] == NUM_BOX && map[playerY + 2 * DIR_ARR[1][dir]][playerX + 2 * DIR_ARR[0][dir]] == NUM_PATH) {
+            ++currMoves;
             map[playerY + 2 * DIR_ARR[1][dir]][playerX + 2 * DIR_ARR[0][dir]] =NUM_BOX;
             gotoxy(2 * (playerX + 2 * DIR_ARR[0][dir]), playerY + 2 * DIR_ARR[1][dir]);
             printf("%s", SYMBLE_BOX);
@@ -196,16 +321,17 @@ void movePlayer(int dir) {
             map[playerY][playerX] = NUM_PLAYER;
             gotoxy(2 * playerX, playerY);
             printf("%s", SYMBLE_PLAYER);
-            if(t == NUM_HOLDER) {
-                if(checkWin()) {
-                    ++currLevel;
-                    showMenu();
-                }
+            printInfo();
+            if(checkWin()) {
+                // score =  caclScore(supposedMoves, currMoves);
+                ++currLevel;
+                dumpScore();
+                showMenu();
             }
-        } 
+            // 有可能box被推到holder上又被推走(或者player从holder上经过)，需要重新打印holder
+            printHolders();
     }
-    // 有可能box被推到holder上又被推走(或者player从holder上经过)，需要重新打印holder
-    resetHolders();
+
 }
 
 void listenKey() {
@@ -225,6 +351,11 @@ void listenKey() {
             case 100:
                 movePlayer(DIR_RIGHT);
                 break;
+            case 114:
+                // restart current level
+                fileReadPosition = preFileReadPosition;
+                initMap();
+                break;    
             default:
                 break;
         }
@@ -235,13 +366,4 @@ void listenKey() {
 void startGame() {
     initMap();
     listenKey();
-}
-
-int main() {
-    consoleInit();
-    startGame();
-
-    printf("\n");
-    system("pause");
-    return 0;
 }
