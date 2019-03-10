d10a3fd823f2d5af2362f20cd2f1f2e0e75fb38b
745745 <41120147+745745@users.noreply.github.com>
2019-03-10 18:22:16 +0800

    汉诺塔
    忘了一个题。。。。

---
+++ 汉诺塔.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+void hanoi(int n, char a, char b, char c);
+int main()
+{
+	int n;
+	cin >> n;
+	hanoi(n, 'a', 'b', 'c');
+
+}
+
+
+void hanoi(int n, char a, char b, char c)
+{
+	if (n == 0)
+	{
+		return;
+	}
+	else
+	{
+		hanoi(n - 1, a, c, b);
+			cout << a << "->" << c<<endl;
+			hanoi(n - 1, b, a, c);
+	}
+
+}
