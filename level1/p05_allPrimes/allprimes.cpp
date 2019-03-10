9f0a7f2afffa23e0a344c9338359e52f6088fe18
zzoxi <961296023@qq.com>
2019-03-10 10:29:51 +0800

    add 02~05

---
+++ level1/p05_allPrimes/allprimes.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+#include<time.h>
+int main(void)
+{
+	int temp;
+	printf("   2   3");
+	for(int i=5;i<=1000;i++)
+	{
+		temp=sqrt(i);
+	
+		for(int j=2;j<=temp;j++)
+		{
+			if(i%j==0)break;	
+			if(j==temp) printf("%4d",i);
+		}
+		
+	}
+printf("\n Time used = %.4f s\n",(double)clock()/CLOCKS_PER_SEC);
+return 0;
+}
