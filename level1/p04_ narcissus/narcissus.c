8d97d234c4794e53bd4a8416d193bbbdf31d37de
louroborus <1908420450@qq.com>
2019-03-13 19:30:08 +0800

    Create narcissus.c
    完成了问题四

---
+++ level1/p04_ narcissus/narcissus.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a, b, c, i;
+	for (i = 100; i < 1000; ++i)
+	{
+		a = i / 100;
+		b = i / 10 - 10 * a;
+		c = i - 100 * a - 10 * b;
+		if (i == a * a*a + b * b*b + c * c*c)
+			printf("%d ", i);
+	}
+}
