c45dff5f832dd88c1ba642dad68b7066e40a3286
louroborus <1908420450@qq.com>
2019-03-13 20:24:20 +0800

    Create allPrimes.cpp
    完成了level1问题05

---
+++ level1/p05_allPrimes/allPrimes.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<time.h>
+#pragma warning(disable:4996)
+int main()
+{
+	int  n;
+	double b, e;
+	b = clock();
+	for (n = 2; n <= 1000; ++n)
+	{
+		int s = 0;
+		for (int i = 2; i*i <= n; ++i)
+		{
+			if (n%i == 0)
+				s++;
+		}
+		if (s == 0)
+			printf("%d ", n);
+	}
+	e = clock();
+	printf("\nTIME:%lfs", (e - b) / CLOCKS_PER_SEC);
+	return 0;
+}
