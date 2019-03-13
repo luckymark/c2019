2a8d2e556ea69adc0351b4fb848872d7fff5cff5
2w6f8c <2428391347@qq.com>
2019-03-13 16:47:02 +0800

    优化代码

--- level1/p05_allPrimes/AllPrimes.c
+++ level1/p05_allPrimes/AllPrimes.c
@@ -2,30 +2,29 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <stdbool.h>
 
-int isPrime(int n) {
-    if (n == 1) return 0;
-    if (n == 2) return 1;
-    if (n % 2 == 0) return 0;
+bool isPrime(int n) {
+    if (n == 1) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false;
 
-    int i;
-    for (i = 3; i <= sqrt(n); i += 2) {
-        if (n % i == 0) return 0;
+    for (int i = 3; i <= sqrt(n); i += 2) {
+        if (n % i == 0) return false;
     }
 
-    return 1;
+    return true;
 }
 
 int main() {
     const int UP = 2000;
-    int i;
     clock_t start, end;
     double totalTime;
 
     start = clock();
 
     printf("%d\t", 2);
-    for (i = 3; i < UP; i += 2) {
+    for (int i = 3; i < UP; i += 2) {
         if (isPrime(i)) printf("%d\t", i);
     }
 
