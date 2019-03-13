80b10d0a92aabce90fd0609a0fcfef75e810374d
2w6f8c <2428391347@qq.com>
2019-03-13 16:41:45 +0800

    引用stdbool;优化代码

--- level1/p02_isPrime/IsPrime.c
+++ level1/p02_isPrime/IsPrime.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdbool.h>
 
-int isPrime(int n) {
+bool isPrime(int n) {
 
-    if(n == 1) return 0;
-    if(n == 2) return 1;
+    if(n == 1) return false;
+    if(n == 2) return true;
 
-    int i;
-    for(i = 2; i <= sqrt(n); i++) {
-        if(n % i == 0) return 0;
+    for(int i = 2; i <= sqrt(n); i++) {
+        if(n % i == 0) return false;
     }
 
-    return 1;
+    return true;
 }
 
 int main() {
