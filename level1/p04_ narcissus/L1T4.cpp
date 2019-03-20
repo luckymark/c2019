bf75807d62f94e9ae467702c185d775604b000c5
lzysb <48143154+lzysb@users.noreply.github.com>
2019-03-20 20:09:45 +0800

    Level 1 Exercise 4

---
+++ level1/p04_ narcissus/L1T4.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int hun,dec,uni,med,nar;
+    for(med=100;med<=999;med++)
+    {  	
+        hun=med/100;
+        dec=(med-(hun*100))/10;
+        uni=(med-(hun*100)-dec*10);
+        nar=hun*hun*hun+dec*dec*dec+uni*uni*uni;
+        if (nar==med)
+        printf("%d ",nar);
+    }
+    printf("\n");
+} 
