cfab8a99e38cb7b62edaa43d28e378b19b38481a
Candiciiii <1249801158@qq.com>
2019-04-03 14:29:20 +0800

    maze

--- git.09/main.cpp
+++ git.09/main.cpp
@@ -6,10 +6,71 @@
 //  Copyright © 2019 兼桑. All rights reserved.
 //
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#include<cstdio>
+#include<cstdlib>
+char a[14][14]={0};
+void maze(){
+    for(int i=1;i<=13;++i)
+    {
+        a[i][1]=1;
+        a[i][13]=1;
+    }
+    for(int j=1;j<=13;++j)
+    {
+        a[1][j]=1;
+        a[13][j]=1;
+    }
+    a[2][10]=a[2][7]=a[2][11]=1;
+    a[3][2]=a[3][3]=a[3][4]=a[3][5]=a[3][7]=a[3][8]=1;
+    a[4][4]=a[4][8]=a[4][11]=a[4][12]=1;
+    a[5][4]=a[5][6]=a[5][8]=a[5][12]=1;
+    a[6][4]=a[6][6]=a[6][9]=a[6][10]=a[6][12]=1;
+    a[7][6]=a[7][9]=a[7][12]=1;
+    a[8][3]=a[8][6]=a[8][8]=a[8][9]=1;
+    a[9][3]=a[9][4]=a[9][5]=a[9][6]=a[9][8]=a[9][11]=1;
+    a[10][6]=a[10][8]=a[10][9]=a[10][11]=1;
+    a[11][2]=a[11][3]=a[11][4]=a[11][6]=a[11][10]=a[11][11]=1;
+    a[12][6]=1;
+    a[13][3]=3;
+    a[2][12]=4;
+    for (int i=1; i<=13; ++i) {
+        for (int j=1; j<=13; ++j) {
+            if(a[i][j]==1)
+            {
+                if(j==13)
+                    printf("■ \n");
+                else printf("■ ");
+            }
+            else if (a[i][j]==0) printf("□ ");
+            else if(a[i][j]==3) printf("△");
+            else printf("※ ");
+        }
+    }
 }
+void move(char act){
+    int i=13,j=3;
+    while(act!='0')
+    {
+        a[i][j]=9;
+        scanf("%c",&act);
+        if(act=='w') a[i-1][j]=9;
+        else if (act=='s') a[i+1][j]=9;
+        else if (act=='a') a[i][j-1]=9;
+        else if (act=='d') a[i][j+1]=9;
+        a[i][j]=0;
+        system("clear");
+        maze();
+        if(a[i][j]==9) printf("△");
+        
+    }
+}
+using namespace std;
+int main()
+{
+    char act='1';
+    maze();
+    printf("now game start! \n△ is where you are now.");
+    move(act);
+    
+}
+
