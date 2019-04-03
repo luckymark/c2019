69f979f9679cc777f1905283d7fab89698c8d218
Candiciiii <1249801158@qq.com>
2019-04-03 14:07:55 +0800

    5

--- git.05/main.cpp
+++ git.05/main.cpp
@@ -6,10 +6,28 @@
 //  Copyright © 2019 兼桑. All rights reserved.
 //
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include<cstdio>
+#include<cstdlib>
+#include<cmath>
+#include<ctime>
+using namespace std;
+int main()
+{
+    clock_t st,et;
+    st=clock();
+    int i=3,j=2;
+    printf("2\n");
+    for(i=3;i<=1000;++i)
+    {
+        for(j=2;j<=sqrt(i);++j)
+        {
+            if(i%j==0)
+                break;
+        }
+        if(j>=sqrt(i))
+            printf("%d\n",i);
+    }
+    et=clock();
+    printf("time is %lu",et-st);
     return 0;
 }
