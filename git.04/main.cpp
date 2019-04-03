6d1ef19bdf2c23725a9ce020d34367201428795a
Candiciiii <1249801158@qq.com>
2019-04-03 14:04:32 +0800

    4

--- git.04/main.cpp
+++ git.04/main.cpp
@@ -6,10 +6,21 @@
 //  Copyright © 2019 兼桑. All rights reserved.
 //
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include<cstdio>
+#include<cstdlib>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int a,b,c,i=100;
+    while(i<1000)
+    {
+        c=i%10;
+        b=i/10%10;
+        a=i/100;
+        if(pow(a,3)+pow(b,3)+pow(c,3)==i)
+            printf("%d\n",i);
+        ++i;
+    }
     return 0;
 }
