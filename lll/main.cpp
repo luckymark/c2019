e96523f2ee6084694856a6620398dbe6a72a382a
Candiciiii <1249801158@qq.com>
2019-04-03 13:58:34 +0800

    2

--- lll/main.cpp
+++ lll/main.cpp
@@ -6,10 +6,30 @@
 //  Copyright © 2019 兼桑. All rights reserved.
 //
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include<cstdio>
+#include<cstdlib>
+#include <cmath>
+using namespace std;
+bool t;
+void fx(int i)
+{
+    for(int j=0;j<=sqrt(i);++j)
+    {
+        if(i%j==0)
+            t=0;
+        else
+            t=1;
+    }
+}
+int main()
+{
+    int i;
+    t=0;
+    printf("input=");
+    scanf("%d",&i);
+    if(t==1)
+        printf("yes");
+    else
+        printf("no");
     return 0;
 }
