3290f3087f700ca6f61875722d372c0f8aed84c5
GhostNoir <48060938+GhostNoir@users.noreply.github.com>
2019-04-03 19:51:06 +0800

    Add files via upload

---
+++ level1/p03_Diophantus/Diophantus.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	for(int age=12;age<=200;++age)//不能再多了吧 
+	{
+		if(age%12==0&&age%7==0)
+		{
+			int x=age/6+age/12+age/7+5+age/2+4;
+			if(x==age)
+			printf("%d",x-4);
+		}
+	}
+	
+	return 0;
+}
