e766926ce6ffd5a3b075177b22b00baab03ab10e
2018081303008 <48054964+2018081303008@users.noreply.github.com>
2019-06-23 21:52:28 +0800

    提交五子棋作业

---
+++ level1/五子棋作业/functions.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void createmap();
+void check_5(char *a[30]);
+void printmap(char *a[30]);
+void deal_live_3(char *a[30]);
+int defense_live_4(char *a[30]);
+int defense_live_3(int n,char *a[30]);
+int attack_live_3_1(int n, char *a[30]);
+int attack_live_3_2(int n, char *a[30]);
+int attack_1(int n,char *a[30]);
+void attack_start(int n,char *a[30]);
